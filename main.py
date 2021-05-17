dea141e6196377eca2caf5ef726dec1438e31d1b
Jannusch Bigge <jannusch.bigge@mailbox.tu-dresden.de>
2021-05-17 23:14:17 +0200

    add comments

--- main.py
+++ main.py
@@ -2,48 +2,59 @@ from scapy.all import *
 import sys
 import binascii
 
+# ask user for input and try to parse to hex and than byte
+# returns the param_list with all params
 def read_params(param_list):
     address = input("Please enter the address:\t")
     if int(address) < 10:
         address = "0" + address
     param_list["address"] = bytes.fromhex(address)
 
-    socket_addr = input("Please enter the socket_addr:\t")
-    if int(socket_addr) < 10:
-        socket_addr = "0" + socket_addr
-    param_list["socket_addr"] = bytes.fromhex(socket_addr)
+    slot = input("Please enter the slot:\t")
+    if int(slot) < 10:
+        slot = "0" + slot
+    param_list["slot"] = bytes.fromhex(slot)
 
     index = input("Please enter the index:\t\t")
     if int(index) < 10:
         index = "0" + index
     param_list["index"] = bytes.fromhex(index)
 
-    print(f"{param_list['framemarker']}{address}{socket_addr}{index}")
+    # print(f"{param_list['framemarker']}{address}{slot}{index}")
     return param_list
 
+# make request to remote proxy via scapy stacking and show answer payload
 def request(param_list):
     print(param_list)
 
-    a = IP(dst="141.76.82.170")/UDP(sport=33333, dport=12345)/Raw(load=param_list["framemarker"] + param_list["address"] + param_list["socket_addr"] + param_list["index"]) # 09 -> keine Ahnung; 06 -> Adresse; 00 -> socket; 01 -> index
+    # build package with scapy stacking method -> IP()/UDP()/Raw()
+    a = IP(dst="141.76.82.170")/UDP(sport=33333, dport=12345)/Raw(load=param_list["framemarker"] + param_list["address"] + param_list["slot"] + param_list["index"]) # 09 -> keine Ahnung; 06 -> Adresse; 00 -> socket; 01 -> index
 
-    awnser = sr1(a)
-    awnser.show()
+    # send and wait for answer
+    answer = sr1(a)
+    answer.show()
 
-    payload = bytes(awnser[Raw]).hex()
+    # convert "raw" answer to readble hex values
+    payload = bytes(answer[Raw]).hex()
     print(payload)
 
 
 if __name__ == "__main__":
     
+    # default param list for pressure sensor
     param_list = {
+        # cretas frammarker with random offset
         "framemarker": bytes([(0xff & random.randint(0x00, 0xff))]),
+        # address for the device ABB Pressure sensor: 06; ABB Temp sensor 07
         "address": bytes([0x06]),
-        "socket_addr": bytes([0x01]),
+        "slot": bytes([0x01]),
         "index": bytes([0x01])
     }
 
+    # if there is an optional cli arg you will be asked for the param_list values
     if len(sys.argv) > 1:
         param_list = read_params(param_list)
 
+    # make request
     request(param_list)
 
