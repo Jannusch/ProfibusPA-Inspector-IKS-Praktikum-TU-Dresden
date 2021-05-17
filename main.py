4f65b146a2ce2b83b1582143af0ec43859cda7ff
Jannusch Bigge <jannusch.bigge@mailbox.tu-dresden.de>
2021-05-17 21:56:39 +0200

    add raw load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from scapy.all import *
 
-a = IP(dst="141.76.82.170")/UDP(dport=12345)
-a.show()
+a = IP(dst="141.76.82.170")/UDP(sport=33333, dport=12345)/Raw(load=bytes.fromhex("09060001")) # 09 -> keine Ahnung; 06 -> Adresse; 00 -> socket; 01 -> index
+# a.show()
 send(a)
