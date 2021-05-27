08020facca6fbd5a9e82e3f570279775cd2dd942
Daniel Kluge <daniel.kluge.98@gmx.de>
2021-05-27 17:07:14 +0200

    Outsource bitstring methods

---
+++ utils.py
@@ -0,0 +1,6 @@
+def bytes_to_bitstring(b) -> str:
+    bitstring = str(bin(int.from_bytes(b, byteorder='big')))[2:]
+    return bitstring.rjust(len(b)*8, "0")
+
+def bitstring_to_int(bs: str) -> int:
+    return int(bs, 2)
