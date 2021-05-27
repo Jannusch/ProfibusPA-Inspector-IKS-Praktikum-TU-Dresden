3191fa7867397df567ceba5d681d98552178de02
Jannusch Bigge <jannusch.bigge@mailbox.tu-dresden.de>
2021-05-27 18:32:24 +0200

    add evaluation composit list directory entries

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ class Device:
 
         raw_payload = bytes(answer[Raw])
         bitstring = bytes_to_bitstring(raw_payload)
+
         self.num_dir_obj = bitstring[40:56]
         self.num_dir_entry = bitstring[56:72]
         self.first_comp_list_dir_entry = bitstring[72:88]
@@ -32,9 +33,44 @@ class Device:
         
         if parse_y_n_input("Show header? [y/n]: "):
             print(f"""Header:\nDir ID: {bitstring_to_int(bitstring[8:24])}\nRev Number: {bitstring_to_int(bitstring[24:40])}\nNum_Dir_Obj: {bitstring_to_int(self.num_dir_obj)}\nNum_Dir_Entry: {bitstring_to_int(self.num_dir_entry)}\nFirst_Comp_List_Dir_Entry: {bitstring_to_int(self.first_comp_list_dir_entry)}\nNum_Comp_List_Dir_Entry: {bitstring_to_int(self.num_comp_list_dir_entry)}""")
-            # print(f"""Header:\nDir ID: {bitstring[0:16]}\nRev Number: {bitstring_to_int(bitstring[16:32])}\nNum_Dir_Obj: {bitstring_to_int(device.num_dir_obj)}\nNum_Dir_Entry: {bitstring_to_int(device.num_dir_entry)}\nFirst_Comp_List_Dir_Entry: {bitstring_to_int(device.first_comp_list_dir_entry)}\nNum_Comp_List_Dir_Entry: {device.num_comp_list_dir_entry}""")
 
+    # make request to remote proxy via scapy stacking and show answer payload
+    def request_composit_list_directory(self):
+        # build package with scapy stacking method -> IP()/UDP()/Raw()
+        framemarker = bytes([(0xff & random.randint(0x00, 0xff))])
+        a = IP(dst="141.76.82.170")/UDP(sport=33333, dport=12345)/Raw(load=framemarker + bytes([self.address]) + bytes([0x01]) + bytes([int(self.num_dir_obj)]))
+
+        # send and wait for answer
+        answer = sr1(a)
+        # answer.show()
+
+        # convert "raw" answer to readble hex values
+        raw_payload = bytes(answer[Raw])
+        # print(raw_payload.hex())
+
+        bitstring = bytes_to_bitstring(raw_payload)
+
+        # Physical Block
+        self.begin_pb = bitstring[8:24]
+        self.no_pb = bitstring[24:40]
         
+        # Transducer Block
+        self.begin_tb = bitstring[40:56]
+        self.no_tb = bitstring[56:72]
+
+        # Function Block
+        self.begin_fb = bitstring[72:88]
+        self.no_fb = bitstring[88:104]
+
+        if parse_y_n_input("Show Composite List Directory Entries? [y/n]: "):
+            print(f"Beging PB:\n\tIndex:\t{hex(bitstring_to_int(self.begin_pb[0:8]))}\n\tOffset:\t{hex(bitstring_to_int(self.begin_pb[8:16]))}")
+            print(f"Number PB: {hex(bitstring_to_int(self.no_pb))}")
+            print(f"Beging TB:\n\tIndex:\t{hex(bitstring_to_int(self.begin_tb[0:8]))}\n\tOffset:\t{hex(bitstring_to_int(self.begin_tb[8:16]))}")
+            print(f"Number PB: {hex(bitstring_to_int(self.no_tb))}")
+            print(f"Beging FB:\n\tIndex:\t{hex(bitstring_to_int(self.begin_fb[0:8]))}\n\tOffset:\t{hex(bitstring_to_int(self.begin_fb[8:16]))}")
+            print(f"Number PB: {hex(bitstring_to_int(self.no_fb))}")
+
+
 
 # ask user for input and try to parse to hex and than byte
 # returns the param_list with all params
@@ -66,78 +102,7 @@ def parse_y_n_input(request) -> bool:
         if answer == "n" or answer == "no":
             return False
         answer = input("Wrong input")
-
-def request_header(device:Device):
-    framemarker = bytes([(0xff & random.randint(0x00, 0xff))])
-    a = IP(dst="141.76.82.170")/UDP(sport=33333, dport=12345)/Raw(load=framemarker + bytes([device.address]) + bytes([0x01]) + bytes([0x00])) # 09 -> keine Ahnung; 06 -> Adresse; 00 -> socket; 01 -> index
-    answer = sr1(a)
-
-    raw_payload = bytes(answer[Raw])
-    bitstring = bytes_to_bitstring(raw_payload)
-    device.num_dir_obj = bitstring[40:56]
-    device.num_dir_entry = bitstring[56:72]
-    device.first_comp_list_dir_entry = bitstring[72:88]
-    device.num_comp_list_dir_entry = bitstring[88:104]
     
-    if parse_y_n_input("Show header? [y/n]: "):
-        print(f"""Header:\nDir ID: {bitstring_to_int(bitstring[8:24])}\nRev Number: {bitstring_to_int(bitstring[24:40])}\nNum_Dir_Obj: {bitstring_to_int(device.num_dir_obj)}\nNum_Dir_Entry: {bitstring_to_int(device.num_dir_entry)}\nFirst_Comp_List_Dir_Entry: {bitstring_to_int(device.first_comp_list_dir_entry)}\nNum_Comp_List_Dir_Entry: {bitstring_to_int(device.num_comp_list_dir_entry)}""")
-        # print(f"""Header:\nDir ID: {bitstring[0:16]}\nRev Number: {bitstring_to_int(bitstring[16:32])}\nNum_Dir_Obj: {bitstring_to_int(device.num_dir_obj)}\nNum_Dir_Entry: {bitstring_to_int(device.num_dir_entry)}\nFirst_Comp_List_Dir_Entry: {bitstring_to_int(device.first_comp_list_dir_entry)}\nNum_Comp_List_Dir_Entry: {device.num_comp_list_dir_entry}""")
-
-    
-
-# make request to remote proxy via scapy stacking and show answer payload
-def request(param_list):
-    # print(param_list)
-
-    # build package with scapy stacking method -> IP()/UDP()/Raw()
-    a = IP(dst="141.76.82.170")/UDP(sport=33333, dport=12345)/Raw(load=param_list["framemarker"] + param_list["address"] + param_list["slot"] + param_list["index"]) # 09 -> keine Ahnung; 06 -> Adresse; 00 -> socket; 01 -> index
-
-    # send and wait for answer
-    answer = sr1(a)
-    # answer.show()
-
-    # convert "raw" answer to readble hex values
-    raw_payload = bytes(answer[Raw])
-    print(raw_payload.hex())
-
-    bitstring = bytes_to_bitstring(raw_payload)
-    print("Framemarker")
-    print(bitstring[0:8]) # Framemarker
-    print(hex(bitstring_to_int(bitstring[0:8]))) # Framemarker but in hex
-    print("\nBegin PB")
-    print(bitstring[8:24])
-    print(hex(bitstring_to_int(bitstring[8:24])))
-    print("unit16: " + str(int('0104', 16)))
-    print("No PB")
-    print(bitstring[24:40])
-    print(bitstring_to_int(bitstring[24:40]))
-
-    
-    print("\nBegin TB")
-    print(bitstring[40:56])
-    print(hex(bitstring_to_int(bitstring[40:56])))
-    print("unint16: " + str(int('0105', 16)))
-    print("No TB")
-    print(bitstring[56:72])
-    print(hex(bitstring_to_int(bitstring[56:72])))
-
-
-    print("\nBegin FB")
-    print(bitstring[72:88])
-    print(hex(bitstring_to_int(bitstring[72:88])))
-    print("unit16: " + str(int('0107', 16)))
-    print("No FB")
-    print(bitstring[88:104])
-    print(hex(bitstring_to_int(bitstring[88:104])))
-
-
-# get specific bits from an byte object
-"""def bits_from_bytes(bytes_object, index, number):
-    # with help and optimizations from https://stackoverflow.com/a/20910950
-    mask = 2 ** number - 1
-    value = int.from_bytes(bytes_object, byteorder='big')
-    
-    return value >> (len(bytes_object) * 8 - (index + number)) & mask"""
 
 def bytes_to_bitstring(b) -> str:
     bitstring = str(bin(int.from_bytes(b, byteorder='big')))[2:]
@@ -149,9 +114,8 @@ def bitstring_to_int(bs: str) -> int:
 if __name__ == "__main__":
 
     device = Device()
-    device.address = 6
+    device.address = 6 # int(input("Pleas enter the address: ")) 
 
-    
     # default param list for pressure sensor
     param_list = {
         # cretas frammarker with random offset
@@ -169,3 +133,4 @@ if __name__ == "__main__":
     # make request
     # request(param_list)
     device.request_header()
+    device.request_composit_list_directory()
