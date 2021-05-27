944aa928163c0d7cefbe31b21656b3f2dd81d185
Jannusch Bigge <jannusch.bigge@mailbox.tu-dresden.de>
2021-05-27 18:05:43 +0200

    add device class and request header

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from scapy.all import *
 import sys
 import binascii
 
+class Device:
+    def __init__(self) -> None:
+        self.address = 0
+        # header
+        self.num_dir_obj = 0
+        self.num_dir_entry = 0
+        self.first_comp_list_dir_entry = 0
+        self.num_comp_list_dir_entry = 0
+        # composit list directory entrie
+        self.begin_pb = 0
+        self.no_pb = 0
+        self.begin_tb = 0
+        self.no_tb = 0
+        self.begin_fb = 0
+        self.no_fb = 0
+        
+
 # ask user for input and try to parse to hex and than byte
 # returns the param_list with all params
 def read_params(param_list):
@@ -23,16 +40,44 @@ def read_params(param_list):
     # print(f"{param_list['framemarker']}{address}{slot}{index}")
     return param_list
 
+def parse_y_n_input(request) -> bool:
+    answer = input(request)
+    answer.lower()
+    while(True):
+        if answer == "y" or answer == "yes" or answer == "j":
+            return True
+        if answer == "n" or answer == "no":
+            return False
+        answer = input("Wrong input")
+
+def request_header(device:Device):
+    framemarker = bytes([(0xff & random.randint(0x00, 0xff))])
+    a = IP(dst="141.76.82.170")/UDP(sport=33333, dport=12345)/Raw(load=framemarker + bytes([device.address]) + bytes([0x01]) + bytes([0x00])) # 09 -> keine Ahnung; 06 -> Adresse; 00 -> socket; 01 -> index
+    answer = sr1(a)
+
+    raw_payload = bytes(answer[Raw])
+    bitstring = bytes_to_bitstring(raw_payload)
+    device.num_dir_obj = bitstring[40:56]
+    device.num_dir_entry = bitstring[56:72]
+    device.first_comp_list_dir_entry = bitstring[72:88]
+    device.num_comp_list_dir_entry = bitstring[88:104]
+    
+    if parse_y_n_input("Show header? [y/n]: "):
+        print(f"""Header:\nDir ID: {bitstring_to_int(bitstring[8:24])}\nRev Number: {bitstring_to_int(bitstring[24:40])}\nNum_Dir_Obj: {bitstring_to_int(device.num_dir_obj)}\nNum_Dir_Entry: {bitstring_to_int(device.num_dir_entry)}\nFirst_Comp_List_Dir_Entry: {bitstring_to_int(device.first_comp_list_dir_entry)}\nNum_Comp_List_Dir_Entry: {bitstring_to_int(device.num_comp_list_dir_entry)}""")
+        # print(f"""Header:\nDir ID: {bitstring[0:16]}\nRev Number: {bitstring_to_int(bitstring[16:32])}\nNum_Dir_Obj: {bitstring_to_int(device.num_dir_obj)}\nNum_Dir_Entry: {bitstring_to_int(device.num_dir_entry)}\nFirst_Comp_List_Dir_Entry: {bitstring_to_int(device.first_comp_list_dir_entry)}\nNum_Comp_List_Dir_Entry: {device.num_comp_list_dir_entry}""")
+
+    
+
 # make request to remote proxy via scapy stacking and show answer payload
 def request(param_list):
-    print(param_list)
+    # print(param_list)
 
     # build package with scapy stacking method -> IP()/UDP()/Raw()
     a = IP(dst="141.76.82.170")/UDP(sport=33333, dport=12345)/Raw(load=param_list["framemarker"] + param_list["address"] + param_list["slot"] + param_list["index"]) # 09 -> keine Ahnung; 06 -> Adresse; 00 -> socket; 01 -> index
 
     # send and wait for answer
     answer = sr1(a)
-    answer.show()
+    # answer.show()
 
     # convert "raw" answer to readble hex values
     raw_payload = bytes(answer[Raw])
@@ -85,6 +130,10 @@ def bitstring_to_int(bs: str) -> int:
     return int(bs, 2)
 
 if __name__ == "__main__":
+
+    device = Device()
+    device.address = 6
+
     
     # default param list for pressure sensor
     param_list = {
@@ -93,7 +142,7 @@ if __name__ == "__main__":
         # address for the device ABB Pressure sensor: 06; ABB Temp sensor 07
         "address": bytes([0x06]),
         "slot": bytes([0x01]),
-        "index": bytes([0x01])
+        "index": bytes([0x00])
     }
 
     # if there is an optional cli arg you will be asked for the param_list values
@@ -101,5 +150,5 @@ if __name__ == "__main__":
         param_list = read_params(param_list)
 
     # make request
-    request(param_list)
-
+    # request(param_list)
+    request_header(device)
