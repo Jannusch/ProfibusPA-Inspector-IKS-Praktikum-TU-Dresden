1582716d2ccb59ccf5832b859ffa8c4194cc46ac
Daniel Kluge <daniel.kluge.98@gmx.de>
2021-06-03 13:26:05 +0200

    Remove PhysicalBlock class, fix cyclic import

--- PhysicalBlockEnums.py
+++ PhysicalBlockEnums.py
@@ -10,17 +10,3 @@ class PhysicalBlockParentClass(IntEnum):
     ANALYSER = 5
     LAB_DEVICE = 6
     MULTI_VARIABLE = 127
-
-
-class PhysicalBlock(Block):
-
-    def __init__(self, bytz):
-        super(bytz)
-
-        if self.block_object_byte != 1:
-            raise ValueError("Block Object must be 0x01 in Physical Block! (got {})".format(self.block_object_byte))
-        if self.class_byte != 250: 
-            raise ValueError("Class must be 0xfa in Physical Block! (got {})".format(self.class_byte))  
-    
-    def get_parent_class(self) -> PhysicalBlockParentClass:
-        return PhysicalBlockParentClass(self.parent_class_byte)
--- TransducerBlockEnums.py
+++ TransducerBlockEnums.py
@@ -1,5 +1,4 @@
 from enum import IntEnum
-from Block import Block
 
 
 class TransducerBlockParentClass(IntEnum):
