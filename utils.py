ae1ceee1d11195890c50572a716ecd821d382f1c
Daniel Kluge <daniel.kluge.98@gmx.de>
2021-06-07 16:32:48 +0200

    Add datatypes

--- utils.py
+++ utils.py
@@ -1,9 +1,35 @@
-from enum import Enum
+from enum import Enum, IntEnum
 
 class PrintLevel(Enum):
     FULL = 0
     ASK = 1
     NOTHING = 2
+
+class DataTypes(IntEnum):
+    FLOAT = 0
+    VALUESTATUS101 = 1
+    VALUESTATUS102 = 2
+    DS36 = 3
+    DS37 = 4
+    DS42 = 5
+    DS49 = 6
+    DS50 = 7
+    DS51 = 8
+    DS60 = 9
+    DS61 = 10
+    DS62 = 11
+    DS63 = 12
+    DS64 = 13
+    DS65 = 14
+    DS66 = 15
+    DS67 = 16
+    DS68 = 17
+    OCTETSTRING = 18
+    UNSIGNED8 = 19
+    UNSIGNED16 = 20
+    FLOATFLOAT = 21
+
+
 
 def bytes_to_bitstring(b) -> str:
     bitstring = str(bin(int.from_bytes(b, byteorder='big')))[2:]
