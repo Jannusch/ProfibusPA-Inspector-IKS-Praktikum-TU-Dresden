3288fa976e4ec9eeb8f9f5ed08d8ae0a707e8525
Jannusch Bigge <jannusch.bigge@mailbox.tu-dresden.de>
2021-06-11 20:20:36 +0200

    add basic flask stuff

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
+import re
 from utils import *
 from Device import Device
 import argparse
 from Block import Block
+import sys
+
+# Webserver
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+@app.route("/init_with_address/", methods = ['POST'])
+def data():
+    if request.method == 'POST':
+        form_data = request.form
+        print(form_data["Field_Address"])
+
+        device = Device(int(form_data["Field_Address"]), PrintLevel.NOTHING)
+    
+        device.request_header()
+        device.request_composit_list_directory()
+
+        return render_template("device.html", device=device)
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description="CLI tool for Profibus PA device inspection")
     parser.add_argument('--address', '-a', metavar="", type=int, help="specifies the address of the device", default=0x06, dest="address")
     parser.add_argument('--print', '-p', choices=["all", "ask", "no"], dest="print", default="ask", help="suppress questions for printing")
+    parser.add_argument('--nowebserver', '-n', dest="server", default=True, help="disable the webserver", action='store_false')
     args = parser.parse_args()
     
     if args.print == "all":
@@ -17,6 +40,12 @@ if __name__ == "__main__":
         printLvl = PrintLevel.NOTHING
     else:
         printLvl = PrintLevel.ASK
+
+    if args.server:
+        app.run()
+        sys.exit(0)
+
+    
 
     device = Device(args.address, printLvl)
     
@@ -30,3 +59,4 @@ if __name__ == "__main__":
     print(hex(bitstring_to_int(block)))
     Block(block, "bit")
     """
+    sys.exit(0)
