284eb300ce2a4615adef278687f82ffc139cf30e
Jannusch Bigge <jannusch.bigge@mailbox.tu-dresden.de>
2021-06-12 17:12:19 +0200

    add README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tool for Profibus PA device inspection
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --address , -a        specifies the address of the device
+  --print {all,ask,no}, -p {all,ask,no}
+                        suppress questions for printing
+  --nowebserver, -n     disable the webserver
--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def data():
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="CLI tool for Profibus PA device inspection")
+    parser = argparse.ArgumentParser(description="Tool for Profibus PA device inspection")
     parser.add_argument('--address', '-a', metavar="", type=int, help="specifies the address of the device", default=0x06, dest="address")
     parser.add_argument('--print', '-p', choices=["all", "ask", "no"], dest="print", default="ask", help="suppress questions for printing")
     parser.add_argument('--nowebserver', '-n', dest="server", default=True, help="disable the webserver", action='store_false')
